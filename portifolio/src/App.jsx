792153fd023d7f21c2cc0f582e144997b64ecccd
manssehtodd <manassehtodd1@gmail.com>
2026-02-23 11:12:53 +0100

    feat: add main components (NavBar, Hero, About, Project, Footer, Work) to App

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,8 +1,18 @@
+import Navbar from "./components/NavBar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Project from "./components/Project"
+import Footer from "./components/Footer"
+
 function App() {
 
   return (
   <div>
-      
+    <NavBar />
+    <Hero />
+    <About />
+    <Project />
+    <Footer />
   </div>)
 }
 
---
+++ portifolio/src/components/About.jsx
@@ -0,0 +1,10 @@
+import Work from "./Work";
+
+function About() {
+    return(
+    <section id="about">
+        <Work />
+    </section>);
+}
+
+export default About;
