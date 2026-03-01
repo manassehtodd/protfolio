7c057be842602fc6aef2923cd4b825a2bd6bb62e
manssehtodd <manassehtodd1@gmail.com>
2026-03-01 08:20:37 +0100

    feat: enhance About component layout and styling; add Work component for experience tracking; include user SVG asset

--- portifolio/src/components/About.jsx
+++ portifolio/src/components/About.jsx
@@ -23,28 +23,28 @@ function About() {
         </div>
 
 
-        <div>
+        <div className="flex flex-row justify-between">
             <div className="grid grid-cols-2 gap-4 w-120 text-[#a6a6a6]">
-                <div className="h-30 w-120 border border-[#a6a6a6] bg-white text-[#121212] rounded-4xl p-5 col-span-2">
+                <div className="h-30 w-120 border border-[#3d3d3d] bg-white text-[#121212] rounded-4xl p-5 col-span-2">
                     <span className="text-xl">Front-end</span>
                     <ul className="flex flex-row gap-2">
                         <li>React /</li>
                         <li>Nextjs /</li>
                     </ul>
                 </div>
-                <div className="h-30 w-50 border border-[#a6a6a6] bg-transparent p-5 rounded-4xl text-[#a6a6a6]">
+                <div className="h-30 w-50 border border-[#3d3d3d] bg-transparent p-5 rounded-4xl text-[#a6a6a6]">
                     <span className="text-xl">Style</span>
                     <ul className="flex flex-row gap-2">
                         <li>Tailwindcss</li>
                     </ul>
                 </div>
                 <div className=" flex flex-row items-center static">
-                    <div className="border border-[#a6a6a6] w-10 rounded-full p-2.5"><BrandGithub className="h-5 w-5" /></div>
-                    <div className="absolute left-74 h-10 w-10 bg-white rounded-full items-center flex justify-center">
-                        <ArrowUpRight className="h-5 w-5" />
+                    <div className="border border-[#3d3d3d] w-10 rounded-full p-2.5"><BrandGithub className="h-5 w-5" /></div>
+                    <div className="absolute left-70 h-10 w-10 bg-white rounded-full items-center flex justify-center">
+                        <ArrowUpRight className="h-5 w-5 cursor-pointer" />
                     </div>
                 </div>
-                <div className="h-30 w-120 border border-[#a6a6a6] bg-transparent p-5 rounded-4xl col-span-2">
+                <div className="h-30 w-120 border border-[#3d3d3d] bg-transparent p-5 rounded-4xl col-span-2">
                     <span className="text-xl">Back-end</span>
                     <ul className="flex flex-row gap-2">
                         <li>Nodejs</li>
@@ -54,13 +54,18 @@ function About() {
                 <div>
                     <span className="text-[#a6a6a6] text-sm">Some of my <em className="text-[#f5f5f5]">favorite technologies or tools</em>, that I worked with</span>
                 </div>
-                <div className="h-30 w-50 border border-[#a6a6a6] bg-transparent p-5 rounded-4xl">
+                <div className="h-30 w-50 border border-[#3d3d3d] bg-transparent p-5 rounded-4xl">
                     <span className="text-xl">DevOps</span>
                     <ul className="flex flex-row gap-2">
                         <li>Git</li>
                         <li>Github</li>
                         <li>Gitlab</li>
                     </ul>
+                </div>
+            </div>
+            <div className="p-10 ml-5">
+                <div className="h-120 w-80 bg-white rounded-xl items-center flex justify-center">
+                    <span className="text-black text-center">PICTURE</span>
                 </div>
             </div>
         </div>
--- portifolio/src/components/Work.jsx
+++ portifolio/src/components/Work.jsx
@@ -1,5 +1,76 @@
+const careerTime = [
+    {
+        years: "2020-2021",
+        start: "2020-01",
+        end: "2021-12",
+        workplace : "Company A",
+        role: "Front-end Developer",
+        tools: "React, Tailwindcss"
+    },
+    {
+        years: "2022-2024",
+        start: "2022-01",
+        end: "2024-05",
+        workplace : "Company B",
+        role: "Front-end Developer",
+        tools: "React, Tailwindcss"
+    },
+    {
+        years: "2024-Present",
+        start: "2024-06",
+        end: "Present",
+        workplace : "Company C",
+        role: "Front-end Developer",
+        tools: "React, Tailwindcss"
+    }
+]
+
+function calculateExperience(careerTime) {
+    let totalMonths = 0;
+
+    careerTime.forEach(job => {
+        const startDate = new Date(job.start);
+        const endDate = job.end === "Present" ? new Date() : new Date(job.end);
+
+        const months = (endDate.getFullYear() - startDate.getFullYear()) * 12 + 
+        (endDate.getMonth() - startDate.getMonth());
+
+        totalMonths += months;
+    });
+
+    const years = Math.floor(totalMonths / 12);
+    const months = totalMonths % 12;
+
+    return `${years} years ${months} months`;
+}
+
+
 function Work() {
-    return;
+
+    const experience = calculateExperience(careerTime);
+
+    return(
+        <div className="p-5">
+            <h1 className="text-7xl text-white font-semibold text-right">Work</h1>
+            <table className="w-full">
+                <caption className="text-right caption-bottom pt-5 text-[#a6a6a6] text-xl mb-2">
+                    Work experiences
+                    <div>
+                    <span className="text-[#f2f2f2] italic text-[18px] ml-2">{experience}</span>
+                    </div>
+                </caption>
+                <tbody className="w-full">
+                    {careerTime.map((item, index) => (
+                        <tr key={index} className="border-b border-[#3d3d3d] hover:bg-[#f2f2f2] hover:text-black  transition-colors duration-300">
+                            <td className="px-4 py-2 pt-6 text-[#a6a6a6]">{item.years}</td>
+                            <td className="px-4 py-2">{item.workplace}</td>
+                            <td className="px-4 py-2">{item.role}</td>
+                            <td className="px-4 py-2">{item.tools}</td>
+                        </tr>))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default Work;
